662e17a68b28eb9fec52c8f2149a479196556250
Rosie Brigham & Sam Johnson <sam.johnson@fundingcircle.com>
2014-03-26 15:15:45 +0000

    Updated tests

--- lib/moon.rb
+++ lib/moon.rb
@@ -6,13 +6,13 @@ class Moon
     @doc = ""
   end
 
-  def moon_cycle_file
-    @doc = Nokogiri::HTML(open("http://www.timeanddate.com/calendar/moonphases.html?year=#{@selected_year}&n=0"))
-    @date = @doc.at_css(moon_cycle_check).content
+  def first_full_moon_date(selected_year)
+    @doc = Nokogiri::HTML(open("http://www.timeanddate.com/calendar/moonphases.html?year=#{selected_year}&n=0"))
+    @date = @doc.at_css(get_correct_css_selector).content
     puts @date
   end
 
-  def moon_cycle_check
+  def get_correct_css_selector
     case @doc.at_css("tbody >:nth-child(1) >:nth-child(6)").content
     when true
       "tbody >:nth-child(1) >:nth-child(6)"
@@ -21,17 +21,25 @@ class Moon
     end
   end
 
-  def year
-    @selected_year = gets.chomp
-    if @selected_year.to_i > 2100
-      puts "#{@selected_year} is too high, please select a lower year"
-      year
+  def get_input_year
+    selected_year = gets.chomp    
+    if vaild_year?(selected_year)
+      first_full_moon_date(selected_year)
     else
-      moon_cycle_file
+      puts "#{selected_year} is invaild, please select a year between 1 and 2100"
+      get_input_year
+    end
+  end
+
+  def vaild_year?(selected_year)
+    if (1..2100).member?(selected_year.to_i) 
+      true
+    else
+      false
     end
   end 
 
 end
-puts "To find out the first full mooon for a year please type the year (up to 2100)"
-Moon.new.year
+# puts "To find out the first full mooon for a year please type the year (up to 2100)"
+# Moon.new.get_input_year
 
--- spec/moon_spec.rb
+++ spec/moon_spec.rb
@@ -4,18 +4,31 @@ require './lib/moon'
 
 describe Moon do
 
-	describe "#moon_cycle_file" do
+	describe "#get_input_year" do
+		it "it asks the user to input a year" do
+      subject.should_receive(:gets).and_return('1900')
+      subject.get_input_year
+		end
 
-		it "will return the http request" do
+    describe "when the input year is valid" do
+      it "it will call first_full_moon_date" do
+        input_year = '1901'
+        subject.stub(:gets).and_return(input_year)
+        subject.should_receive(:vaild_year?).with(input_year).and_return(true)
+        subject.get_input_year
+      end
+    end
 
-			end
 	end
 
-  describe "#moon_cycle_check" do
+  describe "#vaild_year?" do
+    it "returns true is the year is between 1 and 2100" do
+      expect(subject.vaild_year?("1900")).to be true
+    end
 
-    it "will check if top row is true" do
-      subject.
-      end
+    it "returns false is the year is not between 1 and 2100" do
+      expect(subject.vaild_year?("2200")).to be false
+    end
   end
 
 end
